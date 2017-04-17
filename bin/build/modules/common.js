d87dd3af1990c8c6a7c1195aef9872bd4da09819
Atsushi Kawamura <atushi.k@gmail.com>
2017-04-17 17:57:34 +0900

    add bin

---
+++ bin/build/modules/common.js
@@ -0,0 +1,11 @@
+var EXPORTED_SYMBOLS = [ "XULSchool" ];
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+/**
+ * XULSchool namespace.
+ */
+if ("undefined" == typeof(XULSchool)) {
+  var XULSchool = {};
+};
